1473e0a6dd6d7a0044bb6e288b2a9aae479e8ed8
Peter Fenyvesi <peter_peter@external.mckinsey.com>
2020-07-07 00:05:07 +0200

    Added readme

--- index.js
+++ index.js
@@ -1,3 +1,3 @@
-const { CalculateDueDate} = require('./src/calculateDueDate')
+const { CalculateDueDate } = require('./src/calculateDueDate')
 
 console.log(CalculateDueDate(new Date('2020. 07. 07. 15:05'), 1))
---
+++ readme.md
@@ -0,0 +1,12 @@
+# CalculateDueDate
+
+## Usage
+
+```js
+const { CalculateDueDate } = require('./src/calculateDueDate')
+```
+
+## Run tests
+
+1. `npm install`
+2. `npm test`
